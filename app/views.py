c19936547ed7d4675cefd5bcb674ed2a72768309
Software10 <patilaniket04@gmail.com>
2022-01-18 12:00:44 +0530

    Created home page and mobile page

--- app/views.py
+++ app/views.py
@@ -1,10 +1,27 @@
+from unicodedata import category
 from django.shortcuts import render
+from .models import Customer,Product,Cart,OrderPlaced
+from django.views import View
 
-def home(request):
- return render(request, 'app/home.html')
+# def home(request):
+#  return render(request, 'app/home.html')
 
-def product_detail(request):
- return render(request, 'app/productdetail.html')
+class ProductView(View):
+    def get(self, request):
+        topwear = Product.objects.filter(category='TW')
+        bottomwear = Product.objects.filter(category='BM')
+        mobile = Product.objects.filter(category='M')
+        laptop = Product.objects.filter(category='L')
+        return render(request,'app/home.html',{'topwear':topwear,'bottomwear':bottomwear,'mobile':mobile,'laptop':laptop})
+
+# def product_detail(request):
+#     return render(request, 'app/productdetail.html')
+
+
+class ProductDetailView(View):
+    def get(self,request,pk): 
+        product =Product.objects.get(pk=pk)
+        return render(request, 'app/productdetail.html',{'product':product})
 
 def add_to_cart(request):
  return render(request, 'app/addtocart.html')
@@ -24,8 +41,20 @@ def orders(request):
 def change_password(request):
  return render(request, 'app/changepassword.html')
 
-def mobile(request):
- return render(request, 'app/mobile.html')
+def mobile(request,data=None):
+    if data==None:
+        mobiles = Product.objects.filter(category='M')
+    elif data== 'Xiomi' or data== 'Samsung':
+        mobiles = Product.objects.filter(category='M').filter(brand=data)
+    elif data == 'Realme' or data=='Apple':
+        mobiles = Product.objects.filter(category='M').filter(brand=data)
+    elif data == 'above':
+        mobiles = Product.objects.filter(category='M').filter(discounted_price__gt=10000)    
+    
+    elif data == 'below':
+        mobiles = Product.objects.filter(category='M').filter(discounted_price__lt=10000)
+
+    return render(request, 'app/mobile.html',{'mobiles':mobiles})
 
 def login(request):
  return render(request, 'app/login.html')
